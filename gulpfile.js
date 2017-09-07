3810fe9c671dac6ee2c94ef06388ab09cbc7ddb8
Lars Breum Hansen <lars.breum.lh@gmail.com>
2017-09-07 05:51:49 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,31 @@
 'use strict';
-const gulp = require('gulp');
+const gulp = require('gulp'),
+			sass = require('gulp-sass'),
+			webserver = require('gulp-webserver');
 
+//File location variables
+const sassFileLoc = 'src/scss/application.scss';
+const cssLoc = 'src/css'
 
-gulp.task('default', () => {
-		console.log('hello from default');
-	});
+gulp.task('webserver', ['watch'] () => {
+	return gulp.src('src')
+	.pipe(webserver({
+		livereload: {
+			enable: true,
+			}	
+    open: true,
+    fallback: 'index.html',
+	}));
+});
+
+gulp.task('sass', () => {
+  return gulp.src(sassFileLoc)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(cssLoc));
+});
+
+gulp.task('watch', () => {
+  return gulp.watch(sassFileLoc, ['sass']);
+});
+
+gulp.task('default', ['watch']);
