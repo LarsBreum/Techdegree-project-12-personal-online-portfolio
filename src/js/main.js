aaa9c7575a1ae5a2268412701489fb56979988d6
Lars Breum Hansen <lars.breum.lh@gmail.com>
2017-09-11 10:47:19 +0200

    mobile nav somewhat works - but breaks layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-console.log('js works');
+const navTrigger = document.getElementById('nav-trigger');
+const nav = document.querySelector('.navigation');
+const wrapper = document.querySelector('.wrapper');
+
+navTrigger.addEventListener('click', () => {
+	nav.style.right = '25%';
+	wrapper.style.position = 'fixed';
+	wrapper.style.left = '-100%';
+});
