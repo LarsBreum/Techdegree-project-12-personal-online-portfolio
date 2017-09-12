e2da0488abf612faba3503a8a5666b323e47af42
Lars Breum Hansen <lars.breum.lh@gmail.com>
2017-09-12 05:54:30 +0200

    mobile nav working and styled for v1

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
 const navTrigger = document.getElementById('nav-trigger');
 const nav = document.querySelector('.navigation');
-const wrapper = document.querySelector('.wrapper');
+const main = document.querySelector('main');
 
 navTrigger.addEventListener('click', () => {
-	nav.style.right = '25%';
-	wrapper.style.position = 'fixed';
-	wrapper.style.left = '-100%';
+	nav.classList.toggle('navOnCanvas');
+	main.classList.toggle('mainOffCanvas');
 });
