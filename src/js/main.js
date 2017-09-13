6951405feb82ee38032a5c86575ecce178d31c08
Lars Breum Hansen <lars.breum.lh@gmail.com>
2017-09-13 06:21:33 +0200

    better mobile-nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 //nav script
 const navTrigger = document.getElementById('nav-trigger');
 const nav = document.querySelector('.navigation');
-const main = document.querySelectorAll('main, footer');
+const wrapper = document.querySelectorAll('.wrapper, header');
+console.log(wrapper);
 
 navTrigger.addEventListener('click', () => {
 	nav.classList.toggle('navOnCanvas');
 	navTrigger.classList.toggle('change');
 	navTrigger.classList.toggle('navTriggerLeft');
+	for (let i = 0; i < wrapper.length; i++) {
+		wrapper[i].classList.toggle('moveToLeft');
+	}
 });
