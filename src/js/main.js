fbddd4ddd02359db0402d64686718e572263ac3e
Lars Breum Hansen <lars.breum.lh@gmail.com>
2017-09-14 10:28:22 +0200

    sticky nav almost working

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,34 @@
 //nav script
 const navTrigger = document.getElementById('nav-trigger');
-const nav = document.querySelector('.navigation');
+const mobileNav = document.querySelector('.mobile-nav');
 const wrapper = document.querySelectorAll('.wrapper, header');
 
 navTrigger.addEventListener('click', () => {
-	nav.classList.toggle('navOnCanvas');
+	mobileNav.classList.toggle('navOnCanvas');
 	navTrigger.classList.toggle('change');
 	navTrigger.classList.toggle('navTriggerLeft');
 	for (let i = 0; i < wrapper.length; i++) {
 		wrapper[i].classList.toggle('moveToLeft');
 	}
 });
+
+//Sticky header for tablet and desktop
+const nav = document.querySelector('header .navigation');
+const mediaQuery = window.matchMedia( "(min-width: 768px)" );
+if (mediaQuery.matches) {
+	getScrollPos();
+} 
+function getScrollPos() {
+		let scrollPos = window.addEventListener('scroll', (e) => {
+			scrollPos = window.scrollY;
+			console.log(scrollPos);
+			makeNavSticky(scrollPos);
+	});
+}
+function makeNavSticky(scrollPos) {
+	if (scrollPos >= 80) {
+		nav.classList.add('sticky-nav');
+	} else {
+		nav.classList.remove('sticky-nav');
+	}
+}
