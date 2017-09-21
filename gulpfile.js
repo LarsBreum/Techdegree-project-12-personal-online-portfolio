e5a23f90c9fe96e9049f267da91ffeb07a373bb9
Lars Breum Hansen <lars.breum.lh@gmail.com>
2017-09-21 11:42:02 +0200

    gulp automation good, need to make path changes in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
-'use strict';
-const gulp = require('gulp'),
-			sass = require('gulp-sass'),
-			webserver = require('gulp-webserver'),
-			sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const minifyJS = require('gulp-js-minify');
+const rename = require('gulp-rename');
+const htmlmin = require('gulp-htmlmin');
+const image = require('gulp-image');
+const clean = require('gulp-clean');
+const webserver = require('gulp-webserver');
+const inlinesource = require('gulp-inline-source');
+const gutil = require('gulp-util');
 
 //File location variables
 const sassFileLoc = 'src/scss/application.scss';
@@ -19,10 +27,68 @@ gulp.task('webserver', () => {
 	}));
 });
 
-gulp.task('devMode', ['webserver', 'sass:watch'], () => {
-	console.log('Development Mode Activcated!');
+gulp.task('process-css', () => {
+	let plugins = [
+		autoprefixer({
+			browsers: ['last 3 versions']
+		})
+	];
+	return gulp.src('src/css/*.css')
+		.pipe(postcss(plugins))
+		.pipe(cleanCSS())
+		.pipe(concat('dist.min.css'))
+		.pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('default', () => {
-	console.log('no build task specified yet!')
+gulp.task('concatJS', ['clean'], () => {
+	return gulp.src([
+		'src/js/slick.js',
+		'src/js/slick-settings.js',
+		'src/js/nav.js'
+		])
+	.pipe(concat('app.js'))
+	.pipe(gulp.dest('src/js'));
 });
+
+gulp.task('minifyJS', ['concatJS'], function(){
+  return gulp.src('./src/js/app.js')
+    .pipe(minifyJS())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('minifyHTML', ['clean'], () => {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('cleanImg', () => {
+	return gulp.src(['./dist/img'])
+		.pipe(clean())
+});
+
+gulp.task('minifyImg', ['cleanImg'], function () {
+  gulp.src(['src/img/*'])
+    .pipe(image({
+      pngquant: true,
+      optipng: false,
+      zopflipng: true,
+      jpegRecompress: false,
+      mozjpeg: true,
+      guetzli: false,
+      gifsicle: true,
+      svgo: true,
+      concurrent: 10
+    }))
+    .pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('clean', () => {
+	return gulp.src(['./dist/css', './dist/js', './dist/*.html'])
+		.pipe(clean())
+});
+
+gulp.task('build', ['clean', 'process-css', 'concatJS', 'minifyJS', 'minifyHTML'])
+
+gulp.task('default', ['build']);
--- src/js/slick-settings.js
+++ src/js/slick-settings.js
@@ -6,17 +6,17 @@ $(document).ready( () => {
 		arrows: true,
 		lazyload: 'ondemand',
 		autoplay: true,
-  	autoplaySpeed: 2000,
-  	centerMode: true,
-  	variableWidth: true,
-  	responsive: [
+	  	autoplaySpeed: 2000,
+	  	centerMode: true,
+	  	variableWidth: true,
+  		responsive: [
 			{
 				breakpoint: 480,
 				settings: {
 					slidesToShow: 1,
-      		slidesToScroll: 1,
-      		centerMode: false,
-      		arrows: false
+		      		slidesToScroll: 1,
+		      		centerMode: false,
+		      		arrows: false
 				}
   		}
   	]
